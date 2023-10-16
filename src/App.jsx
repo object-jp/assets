2602cac47be60309159483c367da54c16f1d55a6
objectAqua <taigashono0612@gmail.com>
2023-10-16 19:29:43 +0900

    Welcomeの導入

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Nav from "./components/Nav";
+import Welcome from "./components/Welcome"
 import Footer from "./components/Footer";
 
 export default function App() {
@@ -7,11 +8,7 @@ export default function App() {
     <div>
       <Header />
       <Nav />
-      <h2>Welcome</h2>
-
-      <p>Welcome to our fake birdwatching site. If this were a real site, it would be the ideal place to come to learn more about birdwatching, whether you are a beginner looking to learn how to get into birding, or an expert wanting to share ideas, tips, and photos with other like-minded people.</p>
-
-      <p>So don't waste time! Get what you need, then turn off that computer and get out into the great outdoors!</p>
+      <Welcome />
 
       <h2>Favourite photos</h2>
 
