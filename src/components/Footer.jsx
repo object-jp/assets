1b4e62fb3ea08af0af05f6a2cc6718b083ccd442
objectAqua <taigashono0612@gmail.com>
2023-10-16 19:19:39 +0900

    Footerの導入

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <p>
+      This fake website example is CC0 — any part of this code may be reused in any way you wish. Original example written by Chris Mills, 2016.
+      <a href="http://game-icons.net/lorc/originals/dove.html">Dove icon</a> by Lorc.
+    </p>
+  );
+}
