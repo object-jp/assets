ca6ab0d98c9833e1f4ae0187dd354c3144fd7e0f
objectAqua <taigashono0612@gmail.com>
2023-10-16 19:35:29 +0900

    Mainの導入

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./components/Welcome"
+import Gallery from "./components/Gallery"
+
+export default function Main() {
+  return (
+    <main>
+      <Welcome />
+      <Gallery />
+    </main>
+  );
+}
